7d4ed38e92298a38285303d99b16ad81b0688656
amiralikh60 <amir.koshneviszadeh.ext@waterdrop.com>
2023-05-12 15:40:36 +0300

    add logger

---
+++ logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import "fmt"
+
+func Info(data interface{}) {
+	fmt.Println("log info: ", data)
+}
+
+func Error(data interface{}) {
+	fmt.Println("Log error: ", data)
+}
+
+func Warning(data interface{}) {
+	fmt.Println("Warning error: ", data)
+}
